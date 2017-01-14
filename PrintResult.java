e669b2eb94f84f03f83980f973df3dc8ff7f818f
Ashwin Km <kmuraliashwin@gmail.com>
2017-01-14 19:07:41 +0530

    Print values of variables
    Write a program to print the values of variables a,b,c,d,e in separate
    lines

---
+++ PrintResult.java
@@ -0,0 +1,15 @@
+public class PrintResult {
+  public static void main(String args[]){
+    int a=20,b=10;
+    b = (a--)-(--a);
+    int c=a--;
+    int d=a>>2;
+    int e=a&b;
+    System.out.println("a = "+a); 
+    System.out.println("b = "+b);
+    System.out.println("c = "+c);
+    System.out.println("d = "+d);
+    System.out.println("e = "+e);
+   }
+}
+	
